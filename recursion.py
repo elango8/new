00b19277bcfb328663d189e04b64c168b0136981
elango8 <elangoselvan46@gmail.com>
2026-03-17 18:43:09 +0530

    recursion day1

---
+++ recursion.py
@@ -0,0 +1,24 @@
+# def rec(a=0,cut = 0):
+#     if a == 10:
+#         return cut 
+#     else:
+#         cut += a
+#         a+=2
+#         return rec(a,cut)
+    
+# print(rec())
+
+# def num(i=1,n=5):
+#     if i > n:
+#         return 
+#     print(i)
+#     return num(i+1,n)
+# num()
+
+# def num(n=5):
+#     if n <= 0:
+#         return 
+#     print(n)
+#     return num(n-1)
+# num()
+
