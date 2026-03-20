de761e0db5f69201e7dbcbd92df533db1d328dea
elango8 <elangoselvan46@gmail.com>
2026-03-20 20:46:03 +0530

    reverse the array by using the recursion

--- recursion_rev_arr.py
+++ recursion_rev_arr.py
@@ -9,12 +9,12 @@ def rev_ar(ar,l,r):
 ar = [1,2,3,4,5]
 print(rev_ar(ar,0,len(ar)-1))
     
-# def rev_ar(ar, l, r):
-#     if l >= r:
-#         return ar
-#     else:
-#         ar[l], ar[r] = ar[r], ar[l]   
-#         return rev_ar(ar, l+1, r-1)
+def rev_ar(ar, l, r):
+    if l >= r:
+        return ar
+    else:
+        ar[l], ar[r] = ar[r], ar[l]   
+        return rev_ar(ar, l+1, r-1)
 
-# ar = [1, 2, 3, 4, 5]
-# print(rev_ar(ar, 0, len(ar)-1))
+ar = [1, 2, 8, 4, 6]
+print(rev_ar(ar, 0, len(ar)-1))
