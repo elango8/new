034b696580611bba7e135af996436777d36be560
elango8 <elangoselvan46@gmail.com>
2026-04-05 18:27:17 +0530

    Find the First Non-Repeating Element in an Array by using hashing

---
+++ Find the First Non-Repeating Element in an Array.py
@@ -0,0 +1,12 @@
+def first_non_repeating(arr):
+    freq = {}
+    for num in arr:
+        freq[num] = freq.get(num, 0) + 1
+   
+    for num in arr:
+        if freq[num] == 1:
+            return num
+    return -1
+
+
+print(first_non_repeating([4, 5, 1, 2, 0, 4]))  
