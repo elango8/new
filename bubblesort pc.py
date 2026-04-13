ffad4421891543b7635725aef222995fc920be09
elango8 <elangoselvan46@gmail.com>
2026-04-13 18:10:17 +0530

    merge sort practice

---
+++ bubblesort pc.py
@@ -0,0 +1,41 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        print(f"Splitting: {arr} -> Left: {left_half}, Right: {right_half}")
+
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+        print(f"Merging: Left: {left_half}, Right: {right_half} -> {arr}")
+
+
+
+arr = [12, 11, 13, 5, 6, 7]
+print("Original array:", arr)
+merge_sort(arr)
+print("Sorted array:", arr)
