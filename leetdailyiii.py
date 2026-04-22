fa45102c4c53d5e5c0bf9bcd827f7990f21cd51c
elango8 <elangoselvan46@gmail.com>
2026-04-22 20:49:51 +0530

    two edit words problem

---
+++ leetdailyiii.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution:
+    def twoEditWords(self, queries, dictionary):
+        for quer in queries:
+            res= quer
+            for dict in dictionary:
+                cont = set()
+                for ch in dict:
+                    if ch not in cont:
+                        cont.add(ch)
+                if len(quer) != len(dict):
+                    continue
+                co = 2
+                while co <= 0:
+                    for i in range(len(quer)):
+                        if quer[i] == dict[i]:
+                            continue
+                        for k in range(len(cont)):
+                            res[i] = cont[k]
+                            
+                            if quer == dict:
+                                break
+                            else:
+
+queries = ["word","note","ants","wood"]
+dictionary = ["wood","joke","moat"]
+my = Solution()
+print(my.twoEditWords(queries,dictionary))
